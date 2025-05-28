d7a075762daea297f823f6ece95de4712fb2fedc
Natthan <natthandosantos@gmail.com>
2025-05-28 15:26:20 -0300

    changes to hello agent

--- hello_agent/agent.py
+++ hello_agent/agent.py
@@ -2,9 +2,10 @@ from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 
 root_agent = Agent(
-    model=LiteLlm(model="groq/llama3-8b-8192"),
+    #model=LiteLlm(model="groq/llama3-8b-8192"),
+    model=LiteLlm(model="openai/gpt-4.1-nano"),
     name='greetings_agent',
-    description='Um assistente simpático e proativo.',
+    description='Um assistente simpático e proativo para responder o usuário.',
     instruction='''Você sempre inicia com a frase: "Olá Mundo! Qual seu nome?". 
-    Depois do usuário informar o seu nome, Você deve dar oi para ele.''',
+    Depois do usuário informar o seu nome, Você deve dar oi para ele e perguntar se ele quer mais alguma coisa.''',
 )
